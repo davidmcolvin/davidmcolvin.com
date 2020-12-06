af1b402c74284dc6320345f94ca26c0c6938aef3
David Colvin <davidmcolvin@gmail.com>
2020-12-05 21:38:39 -0500

    Start adding state to bubbles

--- js/bubble.js
+++ js/bubble.js
@@ -2,8 +2,28 @@
 
 var BubbleShoot = window.BubbleShoot || {};
 BubbleShoot.Bubble = (function($){
+	BubbleShoot.BubbleState = {
+		CURRENT:1,
+		ON_BOARD:2,
+		FIRING:3,
+		POPPING:4,
+		FALLING:5,
+		POPPED:6,
+		FIRED:7,
+		FALLEN:8
+	};
 	var Bubble = function(row,col,type,sprite){
 		var that = this;
+		var state;
+		var stateStart = Date.now();
+		this.getState = function(){return state;};
+		this.setState = function(stateIn){
+			state = stateIn;
+			startState = Date.now();
+		};
+		this.getTimeInState = function(){
+			return Date.now() - startState;
+		}
 		this.getType = function() {return type;};
 		this.getSprite = function() { return sprite;};
 		this.getCol = function() {return col;};
