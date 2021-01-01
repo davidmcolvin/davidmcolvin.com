741fcc90376604fb57f523efde9569c2a84ca8f4
David Colvin <davidmcolvin@gmail.com>
2020-12-31 21:42:15 -0500

    Fix rows and columns constant

--- js/board.js
+++ js/board.js
@@ -2,8 +2,8 @@
 
 var BubbleShoot = window.BubbleShoot || {};
 BubbleShoot.Board = (function($){
-	var NUM_ROWS = 1;
-	var NUM_COLS = 4;
+	var NUM_ROWS = 9;
+	var NUM_COLS = 32;
 	var Board = function(){
 		var that = this;
 		var rows = createLayout();
